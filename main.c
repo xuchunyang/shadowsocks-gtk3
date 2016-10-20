fb4390890f386d4cbe99a58a4e7e83a0293724da
Chunyang Xu <chunyang@xuchunyang.me>
2016-10-20 19:37:57 +0800

    Use Gtk Radio Check item

--- main.c
+++ main.c
@@ -19,36 +19,73 @@ on_about (GtkMenuItem *menu_item, gpointer user_data)
 }
 
 void
+on_none_proxy (GtkMenuItem *item, gpointer data)
+{
+  if (gtk_check_menu_item_get_active (GTK_CHECK_MENU_ITEM (item)))
+    {
+      g_message ("switching to none proxy mode...");
+    }
+}
+
+void
+on_auto_proxy (GtkMenuItem *item, gpointer data)
+{
+  if (gtk_check_menu_item_get_active (GTK_CHECK_MENU_ITEM (item)))
+    {
+      g_message ("switching to auto proxy mode...");
+    }
+}
+
+void
+on_global_proxy (GtkMenuItem *item, gpointer data)
+{
+  if (gtk_check_menu_item_get_active (GTK_CHECK_MENU_ITEM (item)))
+    {
+      g_message ("switching to global proxy mode");
+    }
+}
+
+void
 activate (GtkApplication *app, gpointer data)
 {
   GtkWidget *window;
   AppIndicator *indicator;
   GtkWidget *indicator_menu;
-  GtkWidget *menu_item;
+  GtkWidget *item;
+  GSList *group = NULL;
 
   window = gtk_application_window_new (app);
   indicator_menu = gtk_menu_new ();
 
+  /* None Proxy Mode */
+  item = gtk_radio_menu_item_new_with_label (group, "None Proxy");
+  group = gtk_radio_menu_item_get_group (GTK_RADIO_MENU_ITEM (item));
+  g_signal_connect (item, "toggled", G_CALLBACK (on_none_proxy), NULL);
+  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), item);
   /* Auto Proxy Mode */
-  menu_item = gtk_check_menu_item_new_with_label ("Auto Proxy Mode");
-  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), menu_item);
-  /* Global Mode */
-  menu_item = gtk_check_menu_item_new_with_label ("Global Mode");
-  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), menu_item);
+  item = gtk_radio_menu_item_new_with_label (group, "Auto Proxy");
+  group = gtk_radio_menu_item_get_group (GTK_RADIO_MENU_ITEM (item));
+  g_signal_connect (item, "toggled", G_CALLBACK (on_auto_proxy), NULL);
+  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), item);
+  /* Global Proxy Mode */
+  item = gtk_radio_menu_item_new_with_label (group, "Global Proxy");
+  group = gtk_radio_menu_item_get_group (GTK_RADIO_MENU_ITEM (item));
+  g_signal_connect (item, "toggled", G_CALLBACK (on_global_proxy), NULL);
+  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), item);
   /* Separator */
-  menu_item = gtk_separator_menu_item_new ();
-  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), menu_item);
+  item = gtk_separator_menu_item_new ();
+  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), item);
   /* About */
-  menu_item = gtk_menu_item_new_with_mnemonic ("About");
-  g_signal_connect (menu_item, "activate", G_CALLBACK (on_about), window);
-  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), menu_item);
+  item = gtk_menu_item_new_with_mnemonic ("About");
+  g_signal_connect (item, "activate", G_CALLBACK (on_about), window);
+  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), item);
   /* Quit */
-  menu_item = gtk_menu_item_new_with_mnemonic ("Quit");
-  g_signal_connect_swapped (menu_item, "activate", G_CALLBACK (g_application_quit), app);
-  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), menu_item);
+  item = gtk_menu_item_new_with_mnemonic ("Quit");
+  g_signal_connect_swapped (item, "activate", G_CALLBACK (g_application_quit), app);
+  gtk_menu_shell_append (GTK_MENU_SHELL (indicator_menu), item);
 
   indicator = app_indicator_new ("shadowsocks-gtk3",
-                                 "starred",
+                                 "sent-mail",
                                  APP_INDICATOR_CATEGORY_APPLICATION_STATUS);
   app_indicator_set_status (indicator, APP_INDICATOR_STATUS_ACTIVE);
   app_indicator_set_menu (indicator, GTK_MENU (indicator_menu));
